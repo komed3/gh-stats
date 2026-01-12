6a871464b0086aa4b704aa40bbaa46b49a9958fe
komed3 <pk97@outlook.de>
2026-01-12 22:28:36 +0100

    use runner() function

--- scripts/fetchProfile.cjs
+++ scripts/fetchProfile.cjs
@@ -1,13 +1,9 @@
 const { ghRequest } = require( '../lib/gh.cjs' );
+const { runner } = require('../lib/runner.cjs');
 const { writeJSON } = require( '../lib/storage.cjs' );
 
-async function fetchProfile () {
+runner( async () => {
     const { data } = await ghRequest( `/user` );
+    if ( ! data ) throw new Error( 'No profile data returned from GitHub API' );
     await writeJSON( 'profile.json', data );
-}
-
-if ( require.main === module ) fetchProfile()
-    .then( () => console.log( 'Profile fetched successfully.' ) )
-    .catch( ( err ) => console.error( 'Error fetching profile:', err ) );
-
-module.exports = fetchProfile;
+} );
