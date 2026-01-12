53f183372689949ab5188e26fe6922ac91c4cf19
komed3 <pk97@outlook.de>
2026-01-12 23:27:05 +0100

    split public & private repos

--- scripts/fetchRepos.cjs
+++ scripts/fetchRepos.cjs
@@ -1,18 +1,22 @@
+const { config } = require( '../lib/config.cjs' );
 const { ghIterate, ghRequest } = require( '../lib/gh.cjs' );
 const { runner } = require( '../lib/runner.cjs' );
 const { writeJSON } = require( '../lib/storage.cjs' );
 
 runner( async () => {
-    const repos = [];
+    const publicRepos = [], privateRepos = [];
 
     for ( const res of await ghIterate( '/user/repos', { per_page: 100, affiliation: 'owner' } ) ) {
         for ( const repo of res.data ) {
             console.log( `... fetching repo: ${ repo.full_name } ...` );
             const { data } = await ghRequest( '/repos/{owner}/{repo}', { owner: repo.owner.login, repo: repo.name } );
             if ( ! data ) throw new Error( `No data returned for repo: ${ repo.full_name }` );
-            repos.push( data );
+
+            if ( ! data.private ) publicRepos.push( data );
+            else if ( config.privateRepos ) privateRepos.push( data );
         }
     }
 
-    await writeJSON( 'repos.json', repos );
+    publicRepos.length && await writeJSON( 'repos.json', publicRepos );
+    privateRepos.length && await writeJSON( 'privateRepos.json', privateRepos );
 } );
