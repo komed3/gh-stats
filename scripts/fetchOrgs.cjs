4eb2d2b50c182bfeb8bf9db9b398234b3e7c605e
komed3 <pk97@outlook.de>
2026-01-12 22:30:40 +0100

    min fix

--- scripts/fetchOrgs.cjs
+++ scripts/fetchOrgs.cjs
@@ -1,18 +1,17 @@
 const { ghIterate, ghRequest } = require( '../lib/gh.cjs' );
+const { runner } = require('../lib/runner.cjs');
 const { writeJSON } = require( '../lib/storage.cjs' );
 
-async function fetchOrgs () {
+await runner( async () => {
     const orgs = [];
 
     for ( const res of await ghIterate( '/user/orgs', { per_page: 100 } ) ) {
-        for ( const org of res.data ) orgs.push(
-            ( await ghRequest( '/orgs/{org}', { org: org.login } ) ).data
-        );
+        for ( const org of res.data ) {
+            const { data } = await ghRequest( '/orgs/{org}', { org: org.login } );
+            if ( ! data ) throw new Error( `No data returned for org: ${ org.login }` );
+            orgs.push( data );
+        }
     }
 
     await writeJSON( 'orgs.json', orgs );
-}
-
-if ( require.main === module ) fetchOrgs();
-
-module.exports = fetchOrgs;
+} );
