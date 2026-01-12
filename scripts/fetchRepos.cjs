1ae0c5b2cbde881e293a44067b38d1df3823f0db
komed3 <pk97@outlook.de>
2026-01-12 23:23:28 +0100

    create fetch repos script

--- scripts/fetchRepos.cjs
+++ scripts/fetchRepos.cjs
@@ -1,9 +1,18 @@
-const {  } = require( '../lib/gh.cjs' );
+const { ghIterate, ghRequest } = require( '../lib/gh.cjs' );
 const { runner } = require( '../lib/runner.cjs' );
 const { writeJSON } = require( '../lib/storage.cjs' );
 
 runner( async () => {
     const repos = [];
+
+    for ( const res of await ghIterate( '/user/repos', { per_page: 100, affiliation: 'owner' } ) ) {
+        for ( const repo of res.data ) {
+            console.log( `... fetching repo: ${ repo.full_name } ...` );
+            const { data } = await ghRequest( '/repos/{owner}/{repo}', { owner: repo.owner.login, repo: repo.name } );
+            if ( ! data ) throw new Error( `No data returned for repo: ${ repo.full_name }` );
+            repos.push( data );
+        }
+    }
 
     await writeJSON( 'repos.json', repos );
 } );
