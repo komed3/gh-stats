43f75ac9fe191431a8ddda26e85760603163781f
komed3 <pk97@outlook.de>
2026-01-13 08:52:30 +0100

    fetch repo languages

---
+++ scripts/fetchLanguages.cjs
@@ -0,0 +1,25 @@
+const { config } = require( '../lib/config.cjs' );
+const { ghIterate, ghRequest } = require( '../lib/gh.cjs' );
+const { runner } = require( '../lib/runner.cjs' );
+const { writeJSON } = require( '../lib/storage.cjs' );
+
+runner( async () => {
+    const langs = {};
+
+    for ( const res of await ghIterate( '/user/repos', { per_page: 100, affiliation: 'owner' } ) ) {
+        for ( const repo of res.data ) {
+            if ( ! config.privateRepos && repo.private ) continue;
+
+            console.log( `... fetching languages for repo: ${ repo.full_name } ...` );
+            const { data } = await ghRequest( '/repos/{owner}/{repo}/languages', { owner: repo.owner.login, repo: repo.name } );
+            if ( ! data || ! data.keys().length ) continue;
+
+            for ( const [ lang, size ] of Object.entries( data ) ) {
+                if ( ! langs[ lang ] ) langs[ lang ] = size;
+                else langs[ lang ] += size;
+            }
+        }
+    }
+
+    await writeJSON( 'languages.json', langs );
+} );
