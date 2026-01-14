1ea314637a8195ac074e32a5ada5b935667ca722
komed3 <pk97@outlook.de>
2026-01-14 11:39:53 +0100

    implement data loader

---
+++ static/app.js
@@ -0,0 +1,11 @@
+const dataCache = new Map();
+
+async function loadData ( path ) {
+    if ( ! dataCache.has( path ) ) {
+        const res = await fetch( `/data/${path}` );
+        if ( ! res.ok ) throw new Error( `Failed to load from path: ${path}` );
+        dataCache.set( path ) = await res.json();
+    }
+
+    return dataCache.get( path );
+}
