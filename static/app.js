062ac4cb5b841bbe23141ce5ac00335bc0a43d0c
komed3 <pk97@outlook.de>
2026-01-14 11:44:04 +0100

    implement DOM manipulator

--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 const dataCache = new Map();
 
-async function loadData ( path ) {
+const loadData = async ( path ) => {
     if ( ! dataCache.has( path ) ) {
         const res = await fetch( `/data/${path}` );
         if ( ! res.ok ) throw new Error( `Failed to load from path: ${path}` );
@@ -8,4 +8,19 @@ async function loadData ( path ) {
     }
 
     return dataCache.get( path );
-}
+};
+
+const $ = ( sel ) => {
+    const el = document.querySelector( sel );
+    return {
+        el,
+        text: ( val ) => ( val !== undefined ? ( el.textContent = val ) : el.textContent ),
+        html: ( val ) => ( val !== undefined ? ( el.innerHTML = val ) : el.innerHTML ),
+        attr: ( name, val ) => ( val !== undefined ? el.setAttribute( name, val ) : el.getAttribute( name ) ),
+        src: ( val ) => ( val !== undefined ? ( el.src = val ) : el.src ),
+        href: ( val ) => ( val !== undefined ? ( el.href = val ) : el.href ),
+        hide: () => ( el.style.display = 'none' ),
+        show: () => ( el.style.display = '' ),
+        link: ( url, text ) => ( $.href( url ).text( text ?? url.replace( /^https?:\/\//, '' ) ) )
+    };
+};
