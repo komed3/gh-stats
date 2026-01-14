5c2242c784b11c5c5fcc0aaa98bf7ed72deaf3e5
komed3 <pk97@outlook.de>
2026-01-14 18:24:22 +0100

    add dashboard contribs info

--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -1,55 +1,60 @@
 document.addEventListener( 'DOMContentLoaded', function () {
-    loadData( 'profile.json' ).then( data => {
+    loadData( 'profile.json' ).then( profile => {
         let s = '.dashboard-profile--', v;
 
-        $( s + 'link' ).href( data.html_url );
-        $( s + 'avatar' ).src( data.avatar_url );
-        $( s + 'name' ).text( data.name || data.login );
-        $( s + 'login' ).text( '@' + data.login );
-        $( s + 'bio' ).text( data.bio || '' );
+        $( s + 'link' ).href( profile.html_url );
+        $( s + 'avatar' ).src( profile.avatar_url );
+        $( s + 'name' ).text( profile.name || profile.login );
+        $( s + 'login' ).text( '@' + profile.login );
+        $( s + 'bio' ).text( profile.bio || '' );
 
-        if ( v = data.location ) $( s + 'location span' ).text( v );
+        if ( v = profile.location ) $( s + 'location span' ).text( v );
         else $( s + 'location' ).hide();
 
-        if ( v = data.blog ) $( s + 'blog a' ).link( v );
+        if ( v = profile.blog ) $( s + 'blog a' ).link( v );
         else $( s + 'blog' ).hide();
 
-        if ( v = data.email ) $( s + 'email a' ).link( 'mailto:' + v, v );
+        if ( v = profile.email ) $( s + 'email a' ).link( 'mailto:' + v, v );
         else $( s + 'email' ).hide();
 
-        if ( v = data.twitter_username ) $( s + 'twitter a' ).link( 'https://twitter.com/' + v, '@' + v );
+        if ( v = profile.twitter_username ) $( s + 'twitter a' ).link( 'https://twitter.com/' + v, '@' + v );
         else $( s + 'twitter' ).hide();
     } ).catch( console.error );
 
-    loadData( 'stats.json' ).then( data => {
+    loadData( 'stats.json' ).then( stats => {
         let s = '.dashboard-stats--';
 
-        $( s + 'contribs b' ).text( fNumber( data.totalContribs, 1 ) );
-        $( s + 'streak b' ).text( fNumber( data.currentStreak?.days ) );
-        $( s + 'level b' ).text( data.ghLevel );
-        $( s + 'reach b' ).text( fNumber( data.socialReach ) );
-        $( s + 'repos b' ).text( fNumber( data.totalPublicRepos ) );
-        $( s + 'stars b' ).text( fNumber( data.totalStars ) );
-        $( s + 'value b' ).text( fNumber( data.estimatedCodingValueUSD, 0, {
+        $( s + 'contribs b' ).text( fNumber( stats.totalContribs, 1 ) );
+        $( s + 'streak b' ).text( fNumber( stats.currentStreak?.days ) );
+        $( s + 'level b' ).text( stats.ghLevel );
+        $( s + 'reach b' ).text( fNumber( stats.socialReach ) );
+        $( s + 'repos b' ).text( fNumber( stats.totalPublicRepos ) );
+        $( s + 'stars b' ).text( fNumber( stats.totalStars ) );
+        $( s + 'value b' ).text( fNumber( stats.estimatedCodingValueUSD, 0, {
             style: 'currency', currency: 'USD', currencyDisplay: 'symbol'
         } ) );
     } ).catch( console.error );
 
-    loadData( 'follower.json' ).then( data => {
+    loadData( 'follower.json' ).then( follower => {
         const { el: container } = $( '.dashboard-followers--grid' );
-        for ( const { login, html_url, avatar_url } of data.slice( 0, 36 ) ) {
-            const el = document.createElement( 'a' );
-            el.href = html_url;
-            el.setAttribute( 'target', '_blank' );
-            el.classList.add( 'item' );
-            el.innerHTML += `<img src="${avatar_url}" alt="GitHub Profile Avatar" />`;
-            el.innerHTML += `<span>${login}</span>`;
-            container.append( el );
+        for ( const { login, html_url, avatar_url } of follower.slice( 0, 36 ) ) {
+            const f = el( 'a', { className: 'item', href: html_url } );
+            f.setAttribute( 'target', '_blank' );
+            f.innerHTML += `<img src="${avatar_url}" alt="GitHub Profile Avatar" />`;
+            f.innerHTML += `<span>${login}</span>`;
+            container.append( f );
         }
     } ).catch( console.error );
 
-    loadData( 'contribs.csv' ).then( data => {
+    loadData( 'contribs.csv' ).then( contribs => {
         const { el } = $( '.dashboard-charts--activity .chart' );
-        el.appendChild( calendar( data ) );
+        el.appendChild( calendar( contribs ) );
+    } ).catch( console.error );
+
+    loadData( 'profile.json', 'radar.json' ).then( ( [ profile, radar ] ) => {
+        $( '.dashboard-charts--info' ).html(
+            `GitHub user <b>@${profile.login}</b> made <b>${ fNumber( radar.total, 0, { notation: 'standard' } ) }</b> ` +
+            `contributions across <b>${ fNumber( radar.repo ) }</b> public repositories, among others, last year.`
+        );
     } ).catch( console.error );
 } );
