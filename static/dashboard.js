834cec7df1049a8a25928a26a656dfff604c2d60
komed3 <pk97@outlook.de>
2026-01-14 12:06:54 +0100

    Update dashboard.js

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,22 +1,23 @@
 document.addEventListener( 'DOMContentLoaded', function () {
     loadData( 'profile.json' ).then( data => {
-        $( '.dashboard-profile--link' ).href( data.html_url );
-        $( '.dashboard-profile--avatar' ).src( data.avatar_url );
-        $( '.dashboard-profile--name' ).text( data.name || data.login );
-        $( '.dashboard-profile--login' ).text( '@' + data.login );
-        $( '.dashboard-profile--bio' ).text( data.bio || '' );
+        let s = '.dashboard-profile--', v;
 
-        let v;
-        if ( v = data.location ) $( '.dashboard-profile--location span' ).text( v );
-        else $( '.dashboard-profile--location' ).hide();
+        $( s + 'link' ).href( data.html_url );
+        $( s + 'avatar' ).src( data.avatar_url );
+        $( s + 'name' ).text( data.name || data.login );
+        $( s + 'login' ).text( '@' + data.login );
+        $( s + 'bio' ).text( data.bio || '' );
 
-        if ( v = data.blog ) $( '.dashboard-profile--blog a' ).link( v );
-        else $( '.dashboard-profile--blog' ).hide();
+        if ( v = data.location ) $( s + 'location span' ).text( v );
+        else $( s + 'location' ).hide();
 
-        if ( v = data.email ) $( '.dashboard-profile--email a' ).link( 'mailto:' + v, v );
-        else $( '.dashboard-profile--email' ).hide();
+        if ( v = data.blog ) $( s + 'blog a' ).link( v );
+        else $( s + 'blog' ).hide();
 
-        if ( v = data.twitter_username ) $( '.dashboard-profile--twitter a' ).link( 'https://twitter.com/' + v, '@' + v );
-        else $( '.dashboard-profile--twitter' ).hide();
+        if ( v = data.email ) $( s + 'email a' ).link( 'mailto:' + v, v );
+        else $( s + 'email' ).hide();
+
+        if ( v = data.twitter_username ) $( s + 'twitter a' ).link( 'https://twitter.com/' + v, '@' + v );
+        else $( s + 'twitter' ).hide();
   } ).catch( console.error );
 } );
