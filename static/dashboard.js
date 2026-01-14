d8bc8106d8cfc655b506998073e3e6c4a0fd2a3d
komed3 <pk97@outlook.de>
2026-01-14 13:49:54 +0100

    load dashboard stats

--- static/dashboard.js
+++ static/dashboard.js
@@ -19,5 +19,19 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
         if ( v = data.twitter_username ) $( s + 'twitter a' ).link( 'https://twitter.com/' + v, '@' + v );
         else $( s + 'twitter' ).hide();
-  } ).catch( console.error );
+    } ).catch( console.error );
+
+    loadData( 'stats.json' ).then( data => {
+        let s = '.dashboard-stats--';
+
+        $( s + 'contribs b' ).text( fNumber( data.totalContribs, 1 ) );
+        $( s + 'streak b' ).text( fNumber( data.currentStreak?.days ) );
+        $( s + 'level b' ).text( data.ghLevel );
+        $( s + 'reach b' ).text( fNumber( data.socialReach ) );
+        $( s + 'repos b' ).text( fNumber( data.totalPublicRepos ) );
+        $( s + 'stars b' ).text( fNumber( data.totalStars ) );
+        $( s + 'value b' ).text( fNumber( data.estimatedCodingValueUSD, 0, {
+            style: 'currency', currency: 'USD', currencyDisplay: 'symbol'
+        } ) );
+    } ).catch( console.error );
 } );
