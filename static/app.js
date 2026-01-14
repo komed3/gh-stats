bb637b2e576c4b13384be715dd134eb90db49390
komed3 <pk97@outlook.de>
2026-01-14 11:40:57 +0100

    min fix

--- static/app.js
+++ static/app.js
@@ -4,7 +4,7 @@ async function loadData ( path ) {
     if ( ! dataCache.has( path ) ) {
         const res = await fetch( `/data/${path}` );
         if ( ! res.ok ) throw new Error( `Failed to load from path: ${path}` );
-        dataCache.set( path ) = await res.json();
+        dataCache.set( path, await res.json() );
     }
 
     return dataCache.get( path );
