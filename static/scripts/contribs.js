e74c581b5dbb87fc0d64ca6dae7d443965af079c
komed3 <pk97@outlook.de>
2026-01-15 14:35:30 +0100

    style contrib stats

--- static/scripts/contribs.js
+++ static/scripts/contribs.js
@@ -3,10 +3,10 @@ document.addEventListener( 'DOMContentLoaded', function () {
         let s = '.contribs-info--stats-';
         $( s + 'avgYear b' ).text( fNumber( stats.avgContribsPerYear, 1 ) );
         $( s + 'avgDay b' ).text( fNumber( stats.avgContribsPerDay, 1 ) );
-        $( s + 'busiestDay b' ).text( fDate( stats.busiestDay.date ) );
-        $( s + 'codeProd b' ).text( fSize( stats.codeProductivity / 1024 ) );
         $( s + 'activeWd b' ).text( [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ][ stats.mostActiveWeekday ] );
         $( s + 'activePeriod b' ).text( stats.mostActivePeriod );
+        $( s + 'codeProd b' ).text( fSize( stats.codeProductivity / 1024 ) );
+        $( s + 'busiestDay b' ).text( fDate( stats.busiestDay.date ) );
 
         contribCharts( $( '.contribs-info--years .chart' ).el, stats.yearlyTotals );
     } ).catch( console.error );
