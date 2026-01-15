79b3e329099465cd1777316f490956ab0d7f2358
komed3 <pk97@outlook.de>
2026-01-15 10:16:32 +0100

    create repo list header

--- static/scripts/repos.js
+++ static/scripts/repos.js
@@ -4,13 +4,18 @@ document.addEventListener( 'DOMContentLoaded', function () {
         repos.forEach( repo => {
             const key = langKey( repo.language );
             const [ owner, name ] = repo.full_name.split( '/' );
-            const r = el( 'div', { className: `box item ${key}` } );
+            const r = el( 'div', { className: `box item repo ${key}` } );
 
-            if ( repo.archived ) r.classList.add( 'archived' );
-            if ( repo.private ) r.classList.add( 'private' );
+            let badge = 'cube', status = undefined;
+            if ( repo.archived ) r.classList.add( 'archived' ), badge = 'file-archive-o', status = 'Archived';
+            else if ( repo.fork ) r.classList.add( 'fork' ), badge = 'code-fork', status = 'Forked';
+            else if ( repo.is_template ) r.classList.add( 'template' ), badge = 'clone', status = 'Template';
+            else if ( repo.private ) r.classList.add( 'private' ), badge = 'lock', status = 'Private';
 
             r.innerHTML = `<div class="repo-header">` +
-                `<h3><i class="fa fa-cube" aria-hidden="true"></i><span>${owner}</span>/<b>${name}</b></h3>` +
+                `<i class="fa fa-${badge} repo-badge" aria-hidden="true"></i>` +
+                `<h3 class="repo-name"><span>${owner}</span>/<a href="${repo.html_url}" target="_blank">${name}</a></h3>` +
+                ( status ? `<span class="repo-status">${status}</span>` : '' ) +
             `</div>`;
 
             container.appendChild( r );
