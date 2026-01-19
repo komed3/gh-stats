a24369770227360e705200379b393e1dec05d6af
komed3 <pk97@outlook.de>
2026-01-20 00:29:15 +0100

    fetchOrgs.cjs migrated to GraphQL

--- scripts/fetchOrgs.cjs
+++ scripts/fetchOrgs.cjs
@@ -1,17 +1,71 @@
-const { ghIterate, ghRequest } = require( '../lib/gh.cjs' );
+const { config } = require( '../lib/config.cjs' );
+const { ghGraphql } = require( '../lib/gh.cjs' );
 const { runner } = require( '../lib/runner.cjs' );
 const { writeJSON } = require( '../lib/storage.cjs' );
 
 runner( async () => {
     const orgs = [];
+    let hasNextPage = true;
+    let endCursor = null;
 
-    for ( const res of await ghIterate( '/user/orgs', { per_page: 100 } ) ) {
-        for ( const org of res.data ) {
+    while ( hasNextPage ) {
+        const query = `
+            query ( $first: Int!, $after: String, $login: String! ) {
+                user( login: $login ) {
+                    organizations( first: $first, after: $after ) {
+                        pageInfo {
+                            hasNextPage
+                            endCursor
+                        }
+                        nodes {
+                            login
+                            name
+                            description
+                            url
+                            avatarUrl
+                            websiteUrl
+                            location
+                            email
+                            twitterUsername
+                            createdAt
+                            repositories {
+                                totalCount
+                            }
+                            membersWithRole {
+                                totalCount
+                            }
+                        }
+                    }
+                }
+            }
+        `;
+
+        const { user } = await ghGraphql( query, { first: 100, after: endCursor, login: config.username } );
+        const { pageInfo, nodes } = user.organizations;
+
+        for ( const org of nodes ) {
             console.log( `... fetching org: ${ org.login } ...` );
-            const { data } = await ghRequest( '/orgs/{org}', { org: org.login } );
-            if ( ! data ) throw new Error( `No data returned for org: ${ org.login }` );
+
+            const data = {
+                login: org.login,
+                name: org.name,
+                description: org.description,
+                url: org.url,
+                avatar_url: org.avatarUrl,
+                blog: org.websiteUrl,
+                location: org.location,
+                email: org.email,
+                twitter_username: org.twitterUsername,
+                created_at: org.createdAt,
+                public_repos: org.repositories.totalCount,
+                public_members: org.membersWithRole.totalCount
+            };
+
             orgs.push( data );
         }
+
+        hasNextPage = pageInfo.hasNextPage;
+        endCursor = pageInfo.endCursor;
     }
 
     await writeJSON( 'orgs.json', orgs );
