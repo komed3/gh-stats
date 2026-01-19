59d97d90d5b7fc0841b1baa1ddcefc80dbc60f36
komed3 <pk97@outlook.de>
2026-01-20 00:28:43 +0100

    fetchLanguages.cjs migrated to GraphQL

--- scripts/fetchLanguages.cjs
+++ scripts/fetchLanguages.cjs
@@ -1,32 +1,69 @@
 const { config } = require( '../lib/config.cjs' );
-const { ghIterate, ghRequest } = require( '../lib/gh.cjs' );
+const { ghGraphql } = require( '../lib/gh.cjs' );
 const { runner } = require( '../lib/runner.cjs' );
 const { writeJSON } = require( '../lib/storage.cjs' );
 
 runner( async () => {
     const langs = {}, relations = {};
+    let hasNextPage = true;
+    let endCursor = null;
 
-    for ( const res of await ghIterate( '/user/repos', { per_page: 100, affiliation: 'owner' } ) ) {
-        for ( const repo of res.data ) {
-            if ( ! config.privateRepos && repo.private ) continue;
+    while ( hasNextPage ) {
+        const query = `
+            query ( $first: Int!, $after: String, $login: String! ) {
+                user( login: $login ) {
+                    repositories( first: $first, after: $after, affiliations: OWNER ) {
+                        pageInfo {
+                            hasNextPage
+                            endCursor
+                        }
+                        nodes {
+                            nameWithOwner
+                            isPrivate
+                            languages( first: 100 ) {
+                                edges {
+                                    size
+                                    node {
+                                        name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `;
 
-            console.log( `... fetching languages for repo: ${ repo.full_name } ...` );
-            const { data } = await ghRequest( '/repos/{owner}/{repo}/languages', { owner: repo.owner.login, repo: repo.name } );
-            if ( ! data || ! Object.keys( data ).length ) continue;
+        const { user } = await ghGraphql( query, { first: 100, after: endCursor, login: config.username } );
+        const { pageInfo, nodes } = user.repositories;
 
-            for ( const [ lang, size ] of Object.entries( data ) ) {
+        for ( const repo of nodes ) {
+            if ( ! config.privateRepos && repo.isPrivate ) continue;
+
+            console.log( `... fetching languages for repo: ${ repo.nameWithOwner } ...` );
+
+            if ( ! repo.languages.edges.length ) continue;
+
+            for ( const edge of repo.languages.edges ) {
+                const lang = edge.node.name;
+                const size = edge.size;
+
                 if ( ! langs[ lang ] ) langs[ lang ] = size;
                 else langs[ lang ] += size;
 
                 if ( ! relations[ lang ] ) relations[ lang ] = {};
 
-                for ( const otherLang of Object.keys( data ) ) {
+                for ( const otherEdge of repo.languages.edges ) {
+                    const otherLang = otherEdge.node.name;
                     if ( otherLang === lang ) continue;
                     if ( ! relations[ lang ][ otherLang ] ) relations[ lang ][ otherLang ] = 1;
                     else relations[ lang ][ otherLang ] += 1;
                 }
             }
         }
+
+        hasNextPage = pageInfo.hasNextPage;
+        endCursor = pageInfo.endCursor;
     }
 
     await writeJSON( 'languages.json', { langs, relations } );
